143b7a64e46a9544d5be3ad8a64a8c7ae54860ff
Nicholas Hilbert <psnail273@gmail.com>
2025-11-30 11:36:14 -0600

    Update with proper posters

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,7 +4,7 @@ import { promises as fs } from 'fs';
 export async function getDetails(path: string) {
   const file = await fs.readFile(`public/${path}/details.json`, 'utf8');
   const data: GalleryImage[] = JSON.parse(file);
-  data.reverse();
+  // data.reverse();
   return data;
 }
 
